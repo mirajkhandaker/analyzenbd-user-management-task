9930f7fb11b96a45aeb251cda586e491d17ec48e
mirajkhandaker <mirajkhandaker@gmail.com>
2024-03-17 22:03:26 +0600

    testing ongoing

--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -6,14 +6,30 @@ use App\Models\User;
 use App\Services\FileService;
 use App\Services\UserService;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\TestCase;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Http\Request;
 
 class UserServiceTest extends TestCase
 {
-    use DatabaseTransactions;
+//    use DatabaseTransactions;
+    use RefreshDatabase;
 
+    public function test_get_all_users()
+    {
+        User::create([
+            'name' => 'test',
+            'email' => 'test@mail.com',
+            'phone_number' => '123456',
+        ]);
+
+        $fileService = new FileService();
+        $userService = new UserService($fileService);
+        $users = $userService->getUsers(false);
+
+        $this->assertCount(1, $users);
+    }
     public function test_create_user()
     {
         $requestData = [
